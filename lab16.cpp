03636cb4904c6580abbb197f6541d893ed1a513e
zyasiawilliams <zyasiaw@umich.edu>
2025-03-31 23:53:32 -0400

    1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,11 @@
-// Code to implement the gcf function goes here
+#include <iostream>
+using namespace std;
+
+int gcf(int A, int B) {
+    if (B == 0) {
+        return A;
+    }
+
+    return gcf(B, A % B);
+}
+
